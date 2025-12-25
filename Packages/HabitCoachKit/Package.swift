38ae4f6142afbf3fe50d3b641a5473a464abbed0
JorgePeCam <jorgeperez1797@gmail.com>
2025-12-25 09:41:20 +0100

    Package renamed

--- Packages/HabitCoachKit/Package.swift
+++ Packages/HabitCoachKit/Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "HabitCoach",
+    name: "HabitCoachKit",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "HabitCoach",
-            targets: ["HabitCoach"]
+            name: "HabitCoachKit",
+            targets: ["HabitCoachKit"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HabitCoach"
+            name: "HabitCoachKit"
         ),
         .testTarget(
-            name: "HabitCoachTests",
-            dependencies: ["HabitCoach"]
+            name: "HabitCoachKitTests",
+            dependencies: ["HabitCoachKit"]
         ),
     ]
 )
